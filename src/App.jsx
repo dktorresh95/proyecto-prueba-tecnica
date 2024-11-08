49220b026ced667df37104bdcc2a8a6e36422215
dktorres1995 <dktorres95@gmail.com>
2024-11-08 11:53:17 -0500

    Se copian archivos filtro, posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+import { Post } from "./Post";
+import { Filter } from "./Filter";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPost] = useState([]);
+  const [tags, setTags] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://dummyapi.io/data/v1/post", {
+          method: "GET",
+          headers: {
+            "app-id": "631251d815c778ad0b7301b6 ",
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Error en la solicitud");
+        }
+        const result = await response.json();
+        setPost(result.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    const fetchDataTag = async () => {
+      try {
+        const response = await fetch("https://dummyapi.io/data/v1/tag", {
+          method: "GET",
+          headers: {
+            "app-id": "631251d815c778ad0b7301b6 ",
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Error en la solicitud");
+        }
+        const result = await response.json();
+        setTags(result.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+
+    
+    fetchData();
+    fetchDataTag();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Filter tags={tags} setPost={setPost}/>
+      <div className="post-page col-12">
+        <div className="row">
+          {posts.map((post) => (
+            <Post key={post.id} props={post} />
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
